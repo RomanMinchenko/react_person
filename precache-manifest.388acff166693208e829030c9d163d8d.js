8fb45f4508fc1104cb19b77838bc0a6d1bcbc0d9
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-27 21:36:37 +0300

    Updates

---
+++ precache-manifest.388acff166693208e829030c9d163d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0bab755acf4913aa2257bd198f7e8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36c708aa9b8d1bf49bf8",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c708aa9b8d1bf49bf8",
+    "url": "./static/js/main.e411031e.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
